bce59810045f2f36a4b7b73c36f7d9e4c7dc4239
holgash <renatocdomingues@outlook.com>
2023-05-25 19:17:58 -0300

    Alterações novas do código

--- lib/project_details_page.dart
+++ lib/project_details_page.dart
@@ -10,28 +10,59 @@ class ProjectDetailsPage extends StatefulWidget {
 }
 
 class _ProjectDetailsPageState extends State<ProjectDetailsPage> {
-  bool _isExpanded = false;
+  bool _isExpandedTop = false;
+  bool _isExpandedUserStory = false;
+  bool _isExpandedBoardManagement = false;
+  bool _isExpandedQualityControl = false;
+
+  // Variáveis para armazenar os valores da aba "User Story Definition"
+  int userStoryCurrent = 15;
+  int userStoryTotal = 25;
+
+  // Variáveis para armazenar os valores da aba "Board Management"
+  int boardManagementCurrent = 20;
+  int boardManagementTotal = 25;
+
+  // Variáveis para armazenar os valores da aba "Quality & Control"
+  int qualityControlCurrent = 40;
+  int qualityControlTotal = 50;
 
   List<Map<String, String>> members = [
     {'name': 'John Doe', 'role': 'Developer'},
+    {'name': 'Tuga', 'role': 'Safado'},
     {'name': 'Jane Smith', 'role': 'Designer'},
     {'name': 'Mike Johnson', 'role': 'Project Manager'},
   ];
+
+  double calculateProgressValue(int current, int total) {
+    return current / total;
+  }
+
+  Color getProgressColor(double progressValue) {
+    if (progressValue < 0.3) {
+      return Colors.red;
+    } else if (progressValue < 0.7) {
+      return Colors.yellow;
+    } else {
+      return Colors.green;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalhes do Projeto'),
+        title: Text('Project Details'),
       ),
-      body: DefaultTabController(
-        length: 3,
+      body: SingleChildScrollView(
+        // Adicionado SingleChildScrollView
         child: Column(
-          children: <Widget>[
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             GestureDetector(
               onTap: () {
                 setState(() {
-                  _isExpanded = !_isExpanded;
+                  _isExpandedTop = !_isExpandedTop;
                 });
               },
               child: Container(
@@ -42,88 +73,229 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage> {
                   children: [
                     Row(
                       children: [
-                        Text(
-                          'Score: 90',
-                          style: TextStyle(fontSize: 18.0),
+                        Container(
+                          width: 120.0,
+                          child: Text(
+                            "Audit score: ${userStoryCurrent + boardManagementCurrent + qualityControlCurrent}/${userStoryTotal + boardManagementTotal + qualityControlTotal}",
+                            style: TextStyle(fontSize: 18.0),
+                          ),
                         ),
                         SizedBox(width: 16.0),
                         Text(
-                          'Sprint Atual: 5',
+                          'Current Sprint: 5',
                           style: TextStyle(fontSize: 18.0),
                         ),
                       ],
                     ),
                     Icon(
-                      _isExpanded ? Icons.expand_less : Icons.expand_more,
+                      _isExpandedTop ? Icons.expand_less : Icons.expand_more,
                       size: 24.0,
                     ),
                   ],
                 ),
               ),
             ),
-            if (_isExpanded)
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Mais informações...',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      SizedBox(height: 16.0),
-                      Text(
-                        'Membros:',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      SizedBox(height: 8.0),
-                      ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: members.length,
-                        itemBuilder: (context, index) {
-                          return Card(
-                            child: ListTile(
-                              title: Text(members[index]['name']!),
-                              subtitle: Text(members[index]['role']!),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+            if (_isExpandedTop)
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 16.0),
+                    Text(
+                      'Members:',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    SizedBox(height: 8.0),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: members.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            title: Text(members[index]['name']!),
+                            subtitle: Text(members[index]['role']!),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'Detalhes do projeto: ${widget.project}',
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isExpandedUserStory = !_isExpandedUserStory;
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                color: Colors.grey[200],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'User Story Definition',
                       style: TextStyle(fontSize: 18.0),
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'Lista de tarefas do projeto: ${widget.project}',
-                      style: TextStyle(fontSize: 18.0),
+                    Icon(
+                      _isExpandedUserStory
+                          ? Icons.expand_less
+                          : Icons.expand_more,
+                      size: 24.0,
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'Membros do projeto: ${widget.project}',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
+            if (_isExpandedUserStory)
+              buildProgressTab(
+                'Score',
+                'Score: $userStoryCurrent/$userStoryTotal',
+                calculateProgressValue(userStoryCurrent, userStoryTotal),
+              ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isExpandedBoardManagement = !_isExpandedBoardManagement;
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                color: Colors.grey[200],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Board Management',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    Icon(
+                      _isExpandedBoardManagement
+                          ? Icons.expand_less
+                          : Icons.expand_more,
+                      size: 24.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            if (_isExpandedBoardManagement)
+              buildProgressTab(
+                'Board Management',
+                'Score: $boardManagementCurrent/$boardManagementTotal',
+                calculateProgressValue(
+                    boardManagementCurrent, boardManagementTotal),
+              ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isExpandedQualityControl = !_isExpandedQualityControl;
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                color: Colors.grey[200],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Quality & Control',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    Icon(
+                      _isExpandedQualityControl
+                          ? Icons.expand_less
+                          : Icons.expand_more,
+                      size: 24.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            if (_isExpandedQualityControl)
+              buildProgressTab(
+                'Quality & Control',
+                'Score: $qualityControlCurrent/$qualityControlTotal',
+                calculateProgressValue(
+                    qualityControlCurrent, qualityControlTotal),
+              ),
           ],
         ),
       ),
     );
   }
+
+  Widget buildProgressTab(
+      String title, String progressText, double progressValue) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: TextStyle(fontSize: 18.0),
+              ),
+              Text(
+                progressText,
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ],
+          ),
+        ),
+        LinearProgressIndicator(
+          value: progressValue,
+          backgroundColor: Colors.grey[300],
+          valueColor: AlwaysStoppedAnimation<Color>(
+            getProgressColor(progressValue),
+          ),
+        ),
+        SizedBox(height: 16.0),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'User Story Size',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'Small',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Tasks:',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              SizedBox(height: 8.0),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text('Task ${index + 1}'),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ProjectDetailsPage('My Project'),
+  ));
 }
