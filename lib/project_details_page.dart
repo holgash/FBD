bb79ee1e57f93efafd15a0bcaea7325d8819c2a2
holgash <renatocdomingues@outlook.com>
2023-05-30 18:15:04 -0300

    Merge changes from remote repository

--- lib/project_details_page.dart
+++ lib/project_details_page.dart
@@ -54,7 +54,6 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage> {
         title: Text('Project Details'),
       ),
       body: SingleChildScrollView(
-        // Adicionado SingleChildScrollView
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -147,11 +146,128 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage> {
                 ),
               ),
             ),
+            /*
+            /////////////////////////////////////////////////////////////////////////// 
+            Aqui começa a aba User Story Definition expandida
+            /////////////////////////////////////////////////////////////////////////// 
+            */
             if (_isExpandedUserStory)
-              buildProgressTab(
-                'Score',
-                'Score: $userStoryCurrent/$userStoryTotal',
-                calculateProgressValue(userStoryCurrent, userStoryTotal),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+                child: buildProgressTabIndicator(
+                  userStoryCurrent.toDouble(),
+                  userStoryTotal.toDouble(),
+                  'User Story Definition',
+                ),
+              ),
+            if (_isExpandedUserStory)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  children: [
+                    buildProgressTabText(
+                      'User Story Size',
+                      'Size of Original Time Estimate (OTE) of estimated issues',
+                    ),
+                    generateContainer(
+                      'OTE > 3 days',
+                      'No penalty',
+                      '0%(No penalty)',
+                      '0%',
+                    ),
+                    generateContainer(
+                      'OTE > 2 days & >= 3 days',
+                      'No penalty',
+                      '<=10%(No penalty)',
+                      '5%',
+                    ),
+                    generateContainer(
+                      'OTE <= 2 days',
+                      'No penalty',
+                      '>=90%(No penalty)',
+                      '95%',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedUserStory)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildProgressTabText(
+                      'Sample User story 1',
+                      'key-001',
+                    ),
+                    generateContainer(
+                      'Acceptance Criteria ',
+                      'penalty',
+                      'unfilled',
+                    ),
+                    generateContainer(
+                      'Definiton of Ready',
+                      'No penalty',
+                      'filled',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedUserStory)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildProgressTabText(
+                      'Sample User story 2',
+                      'key-002',
+                    ),
+                    generateContainer(
+                      'Acceptance Criteria ',
+                      'penalty',
+                      'unfilled',
+                    ),
+                    generateContainer(
+                      'Definiton of Ready',
+                      'No penalty',
+                      'filled',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedUserStory)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildProgressTabText(
+                      'Sample User story 3',
+                      'key-003',
+                    ),
+                    generateContainer(
+                      'Acceptance Criteria ',
+                      'penalty',
+                      'unfilled',
+                    ),
+                    generateContainer(
+                      'Definiton of Ready',
+                      'No penalty',
+                      'filled',
+                    ),
+                  ],
+                ),
               ),
             GestureDetector(
               onTap: () {
@@ -179,12 +295,130 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage> {
                 ),
               ),
             ),
+            /*
+            /////////////////////////////////////////////////////////////////////////// 
+            Aqui começa a aba Board Management expandida
+            /////////////////////////////////////////////////////////////////////////// 
+            */
             if (_isExpandedBoardManagement)
-              buildProgressTab(
-                'Board Management',
-                'Score: $boardManagementCurrent/$boardManagementTotal',
-                calculateProgressValue(
-                    boardManagementCurrent, boardManagementTotal),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+                child: buildProgressTabIndicator(
+                  boardManagementCurrent.toDouble(),
+                  boardManagementTotal.toDouble(),
+                  'Board Management',
+                ),
+              ),
+            if (_isExpandedBoardManagement)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildProgressTabText(
+                      'Sprint Goal Definition',
+                      'Sprint goal definition field is filled',
+                    ),
+                    generateContainer(
+                      'Sprint Goal Definition',
+                      'No penalty',
+                      'Filled',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedBoardManagement)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  children: [
+                    buildProgressTabText(
+                      'Backlog prioritization',
+                      'Backlog is prioritized with “Must haves”/highest priority, “Should”/high priority and “Could”/Medium priority items.',
+                    ),
+                    generateContainer(
+                      'Prioritizated',
+                      'No penalty',
+                      'No penalty',
+                    ),
+                    generateContainer(
+                      'Medium != 100%',
+                      'No penalty',
+                      'No penalty',
+                      '80%',
+                    ),
+                    generateContainer(
+                      'High <= 80%',
+                      'No penalty',
+                      'No penalty',
+                      '10%',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedBoardManagement)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  children: [
+                    buildProgressTabText(
+                      'Defects',
+                      'Defects classification, prioritization and estimation (estimation applies only to Major defects)',
+                    ),
+                    generateContainer(
+                      'Classified',
+                      'penalty',
+                      'Full penalty',
+                      'Not',
+                    ),
+                    generateContainer(
+                      'Prioritizated',
+                      'penalty',
+                      'Full penalty',
+                      'Not',
+                    ),
+                    generateContainer(
+                      'Estimated',
+                      'penalty',
+                      'Full penalty',
+                      'Not',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedBoardManagement)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  children: [
+                    buildProgressTabText(
+                      'Front End + Design Issues',
+                      'Exists issues from category Front End and/or Design',
+                    ),
+                    generateContainer(
+                      'Front End',
+                      'No penalty',
+                      'Exists',
+                      '10',
+                    ),
+                    generateContainer(
+                      'Design',
+                      'No penalty',
+                      'Exists',
+                      '5',
+                    ),
+                  ],
+                ),
               ),
             GestureDetector(
               onTap: () {
@@ -212,12 +446,81 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage> {
                 ),
               ),
             ),
+            /*
+            /////////////////////////////////////////////////////////////////////////// 
+            Aqui começa a aba Quality & Control expandida
+            /////////////////////////////////////////////////////////////////////////// 
+            */
             if (_isExpandedQualityControl)
-              buildProgressTab(
-                'Quality & Control',
-                'Score: $qualityControlCurrent/$qualityControlTotal',
-                calculateProgressValue(
-                    qualityControlCurrent, qualityControlTotal),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+                child: buildProgressTabIndicator(
+                  qualityControlCurrent.toDouble(),
+                  qualityControlTotal.toDouble(),
+                  'Quality & Control',
+                ),
+              ),
+            if (_isExpandedQualityControl)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildProgressTabText(
+                      'Sprint Definition',
+                      'Accuracy = # of US on “UAT ready” status/ # of US',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedQualityControl)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildProgressTabText(
+                      'Issue estimation',
+                      'Accuracy = # of estimated issues / # of total issues',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedQualityControl)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildProgressTabText(
+                      'Project execution',
+                      'Accuracy = 100% - |∑total time / ∑estimated| (precise description pending)',
+                    ),
+                  ],
+                ),
+              ),
+            if (_isExpandedQualityControl)
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildProgressTabText(
+                      '# of Bugs in quality',
+                      '# of Bugs / # of User Stories',
+                    ),
+                  ],
+                ),
               ),
           ],
         ),
@@ -225,76 +528,116 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage> {
     );
   }
 
-  Widget buildProgressTab(
-      String title, String progressText, double progressValue) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          padding: EdgeInsets.all(16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                title,
-                style: TextStyle(fontSize: 18.0),
-              ),
-              Text(
-                progressText,
-                style: TextStyle(fontSize: 18.0),
-              ),
-            ],
+/*
+/////////////////////////////////////////////////////////////////////////// 
+Aqui começam as funções
+/////////////////////////////////////////////////////////////////////////// 
+*/
+  Widget buildProgressTabText(
+    String sizeTitle,
+    String sizeDescription,
+  ) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 8.0),
+          Text(
+            sizeTitle,
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
           ),
-        ),
-        LinearProgressIndicator(
-          value: progressValue,
-          backgroundColor: Colors.grey[300],
-          valueColor: AlwaysStoppedAnimation<Color>(
-            getProgressColor(progressValue),
+          SizedBox(height: 8.0),
+          Text(
+            sizeDescription,
+            style: TextStyle(fontSize: 16.0),
           ),
-        ),
-        SizedBox(height: 16.0),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'User Story Size',
-                style: TextStyle(fontSize: 18.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                'Small',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'Tasks:',
-                style: TextStyle(fontSize: 18.0),
-              ),
-              SizedBox(height: 8.0),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: 3,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      title: Text('Task ${index + 1}'),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
-}
 
-void main() {
-  runApp(MaterialApp(
-    home: ProjectDetailsPage('My Project'),
-  ));
+  Widget buildProgressTabIndicator(
+      double progressValue, double progressTotal, String title) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          SizedBox(height: 16.0),
+          Text(
+            title,
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          LinearProgressIndicator(
+            value: progressValue / progressTotal,
+            valueColor: AlwaysStoppedAnimation<Color>(
+                getProgressColor(progressValue / progressTotal)),
+            backgroundColor: Colors.grey[300],
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            '${(progressValue.toInt())}/${(progressTotal.toInt())}',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget generateContainer(String text1, String text2, String text3,
+      [String? optionalText]) {
+    Color circleColor;
+
+    if (text2 == "penalty") {
+      circleColor = Colors.red;
+    } else if (text2 == "partially") {
+      circleColor = Colors.yellow;
+    } else if (text2 == "No penalty") {
+      circleColor = Colors.green;
+    } else {
+      circleColor = Colors.transparent;
+    }
+
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              text1,
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+          SizedBox(width: 8.0),
+          CircleAvatar(
+            radius: 8.0,
+            backgroundColor: circleColor,
+          ),
+          SizedBox(width: 8.0),
+          Expanded(
+            child: Text(
+              text3,
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+          if (optionalText != null && optionalText.isNotEmpty) ...[
+            SizedBox(width: 8.0),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+              width: 100.0, // Defina o tamanho fixo desejado aqui
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  optionalText,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
 }
